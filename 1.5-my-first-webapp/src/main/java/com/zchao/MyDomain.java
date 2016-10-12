24767db7841f2d12ec78d79edf6ddeaf033e2437
zchao <hao.sealter@gmail.com>
2016-10-13 01:30:05 +0800

    my first webapp

---
+++ 1.5-my-first-webapp/src/main/java/com/zchao/MyDomain.java
@@ -0,0 +1,40 @@
+package com.zchao;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Created by zchao on 2016/10/13.
+ */
+@XmlRootElement
+public class MyDomain {
+
+    private String name;
+    private String value;
+
+    public MyDomain() {
+    }
+
+    public MyDomain(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    @XmlAttribute
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @XmlAttribute
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
